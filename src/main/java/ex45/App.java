30d9b2797cd3290e51aa11a4164053e308140021
Joey-Crown <joe@thecrownfamily.com>
2021-06-18 21:52:34 -0400

    finished exercise 45

--- src/main/java/ex45/App.java
+++ src/main/java/ex45/App.java
@@ -36,6 +36,13 @@ Modify the program so it can handle every file a folder of files instead of a si
 
  */
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class App {
 
     /*
@@ -49,6 +56,61 @@ public class App {
      */
 
     public static void main(String[] args) {
+        String path = "src/main/java/ex45/exercise45_input.txt";    // input file path
+        String outputFile = "src/main/java/ex45/" + getUserInput(); // user named output file
+        System.out.println(createFile(outputFile));                 // create output file
+        String find = readFromFile(path);                           // read file into string
+        if (find != null) {
+            System.out.println(writeToFile(find, outputFile));      // replace words and output to file
+        }
+    }
 
+    // takes input file path and reads text into the string 'data'
+    public static String readFromFile(String path) {
+        try {
+            String data = new String(Files.readAllBytes(Paths.get(path)));
+            return data;
+        } catch (IOException e) {
+            System.out.println("Failure reading file");
+        }
+        return null;
+    }
+
+    // replaces all instances of a word in the string with another word
+    // writes edited string to output file
+    public static String writeToFile(String find, String outputFile) {
+        String replace = find.replaceAll("utilize", "use");
+        try {
+            FileWriter fileWriter = new FileWriter(outputFile);
+            fileWriter.write(replace);
+            fileWriter.close();
+
+        } catch (IOException e) {
+            return "Error writing to file.";
+        }
+
+        return "File output successful, 'utilize' replaced with 'use'.";
+    }
+
+    // gets user input for name of output file
+    public static String getUserInput(){
+        Scanner input = new Scanner(System.in);
+
+        //input output file name
+        System.out.print("What is the name of the output file? ");
+        return input.nextLine();
+    }
+
+    // creates file in /ex45/ according to string provided
+    public static String createFile(String outputFile){
+        try {
+            File file = new File(outputFile);
+            if (file.createNewFile()) {
+                return "File successfully created.";
+            }
+        } catch (IOException e) {
+            return "Error creating file.";
+        }
+        return "File already created.";
     }
 }
